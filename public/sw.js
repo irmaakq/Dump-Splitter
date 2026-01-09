fb7e20a8ff8ac91ed6b3f6d650491680ed3c9500
ırmak <54485394+irmaakq@users.noreply.github.com>
2026-01-10 02:19:37 +0300

    PWA dosyaları eklendi

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'dump-splitter-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/style.css', // Eğer stil dosyanın adı farklıysa düzelt
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
